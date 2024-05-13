65650a3de40ecc34c57ff591dbc1e9235458dab8
ArtyWhit <julwer1k@gmail.com>
2024-05-13 23:12:03 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,33 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    if (Object.keys(data).length === 0) return;
+    const list = document.createElement("ul");
+    Object.entries(data).forEach(([key, value])=>{
+        const item = document.createElement("li");
+        item.textContent = key;
+        list.appendChild(item);
+        createTree(item, value);
+        list.appendChild(item);
+    });
+    element.appendChild(list);
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
